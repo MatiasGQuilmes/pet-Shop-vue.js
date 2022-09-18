4d838c52192582dff8e46e3001874ddcc57056e6
Pablo ramos <benjapabloramos2002@gmail.com>
2022-09-18 12:09:07 -0300

    filtro cruzado config

--- javascript/vue.js
+++ javascript/vue.js
@@ -1,15 +1,19 @@
-const { createApp } = Vue
-
-createApp({
+const app = Vue.createApp({
   data() {
     return {
         products: [],
+        backupProducts: [],
         juguetes:[],
         farmacia:[],
-        message: "hola",
-        api:"https://apipetshop.herokuapp.com/api/articulos"
-        
+        checkbox:[],
+        api:"https://apipetshop.herokuapp.com/api/articulos",
 
+        id: new URLSearchParams(location.search).get("id"),
+        textSearch: "", 
+        detailProduct: [],
+	test:{},
+	category:[]
+	
     }
   },
   created(){
@@ -20,29 +24,44 @@ createApp({
         fetch(url)
         .then(response => response.json()
             .then(data => {
-                this.products = data.response
-              console.log(products)
+              this.products = data.response
+              this.backupProducts = this.products
+
+              this.detailProduct = this.products.find( e => e._id == this.id)
+
+
+
 
 
 
             })
         ).catch(err => console.log(err.message))
-    }
+ 
+
+
+
+    },
+
 
 
   },
   computed:{
-  /*   filtroSearch(){
-        let filtroSearch = this.products.filter(product => product.name.toLowerCase().includes(this.textSearch.toLowerCase()))    
-        if(this.filterCategory.length){
-            this.products = checkDesk.filter(e => this.filterCategory.includes(e.category))
-        }else{
-            this.products = primerFiltro
-           
-    } */
+      filtroSearch(){
+	let search = this.backupProducts.filter(product => product.nombre.toLowerCase().includes(this.textSearch.toLowerCase()))
+	      console.log();
+	      if (this.checkbox.length > 0) {
+		let checkbox = this.backupProducts.filter(product => product.tipo.includes(this.checkbox.at(-1)))
+			this.products= search.filter(products => {return checkbox.includes(products)})
+		 
+	 }
+	      else{
+		      this.products = this.backupProducts
+	      }
 
 
-    
+      },
+	
+
   }
 
 }).mount('#app')
